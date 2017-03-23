7bcc59b0d5f8ea33d41af46b47853c8685005605
WarriorUA <dinozawrua@gmail.com>
2017-03-23 20:25:12 +0200

    ы

--- classTrap/ClassPoint.cs
+++ classTrap/ClassPoint.cs
@@ -8,17 +8,17 @@ using System.Windows.Forms;
 
 namespace classTrap
 {
-    internal abstract class ClassPoint
+    internal abstract class ClassPoint//абстрактнй класс
     {
         public const int countPoints = 4;
         public Point[] points;
-        public abstract void setPoints();
+        public abstract void setPoints();//абстрактный метод
     }
 
     class Trap : ClassPoint
     {
         public float far = 0;
-        public override void setPoints()
+        public override void setPoints()//полиморфизм, перегруженный метод
         {
             points = new Point[countPoints];
             points[0].X = 0;
@@ -30,19 +30,19 @@ namespace classTrap
             points[3].X = 100;
             points[3].Y = 0;
         }
-        public void Paint(object sender, PaintEventArgs e)
+        public void Paint(object sender, PaintEventArgs e)//рисование
         {
             Graphics g = e.Graphics;
             g.Clear(Color.White);
             g.FillPolygon(new SolidBrush(Color.Aqua), points);
         }
-        public void Rotate(object sender, PaintEventArgs e)
+        public void Rotate(object sender, PaintEventArgs e)//поворот
         {
             far += 30;
             Graphics g = e.Graphics;
             g.Clear(Color.White);
-            e.Graphics.RotateTransform(far);
-            e.Graphics.TranslateTransform(100.0F, 0.0F);
+            e.Graphics.RotateTransform(far);//угол поворота
+            e.Graphics.TranslateTransform(100.0F, 0.0F);//поворот, в остальных поворотах все то же
             g.DrawPolygon(new Pen(Color.Black, 2), points);
         }
     }
@@ -50,23 +50,23 @@ namespace classTrap
     class Chetur : ClassPoint
     {
         public float far = 0;
-        public override void setPoints()
+        public override void setPoints()//полиморфизм, перегруженный метод
         {
             points = new Point[countPoints];
         }
-        public void Draw(object sender, PaintEventArgs e)
+        public void Draw(object sender, PaintEventArgs e)//рисование
         {
             Graphics g = e.Graphics;
             g.Clear(Color.White);
             g.DrawPolygon(new Pen(Color.Black,2),points );
         }
-        public void Pouring(object sender, PaintEventArgs e)
+        public void Pouring(object sender, PaintEventArgs e)//закрашивание
         {
             Graphics g = e.Graphics;
             g.Clear(Color.White);
             g.FillPolygon(new SolidBrush(Color.Aqua), points);
         }
-        public void RotatePaint(object sender, PaintEventArgs e)
+        public void RotatePaint(object sender, PaintEventArgs e)//поворот нарисованной трапеции
         {
             far += 30;
             Graphics g = e.Graphics;
@@ -75,7 +75,7 @@ namespace classTrap
             e.Graphics.TranslateTransform(100.0F, 0.0F);
             g.DrawPolygon(new Pen(Color.Black, 2), points);
         }
-        public void RotatePouring(object sender, PaintEventArgs e)
+        public void RotatePouring(object sender, PaintEventArgs e)//поворот закрашенной трапеции
         {
             far += 30;
             Graphics g = e.Graphics;
@@ -84,7 +84,7 @@ namespace classTrap
             e.Graphics.TranslateTransform(100.0F, 0.0F);
             g.FillPolygon(new SolidBrush(Color.Aqua), points);
         }
-        public void Clear(object sender, PaintEventArgs e)
+        public void Clear(object sender, PaintEventArgs e)//метод очистки
         {
             Graphics g = e.Graphics;
             g.Clear(Color.White);
